bbca3c61b22df6537b5415e978457705d92b6309
LinggarRam <123456789+odin@users.noreply.github.com>
2026-04-20 00:39:06 +0700

    Add book constructor and addBookToLibrary function

--- Library-App/assets/js/main.js
+++ Library-App/assets/js/main.js
@@ -1,1 +1,22 @@
 console.log("Hallo");
+
+const myLibrary = []
+
+function Book (title, author, pages, read) {
+    this.id = crypto.randomUUID();
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+function addBookToLibrary(title, author, pages, read) {
+    const newBook = new Book(title, author, pages, read);
+    myLibrary.push(newBook);
+}
+
+addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 310, true);
+addBookToLibrary("Harry Potter", "J.K. Rowling", 500, false);
+addBookToLibrary("Clean Code", "Robert C. Martin", 464, true);
+
+console.log(myLibrary);
