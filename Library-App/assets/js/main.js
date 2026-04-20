76a4bed2ed2e2ec2f758af4ea159e0fd24321059
LinggarRam <123456789+odin@users.noreply.github.com>
2026-04-20 12:14:21 +0700

    Add displayBooks function and book card styling

--- Library-App/assets/js/main.js
+++ Library-App/assets/js/main.js
@@ -15,8 +15,30 @@ function addBookToLibrary(title, author, pages, read) {
     myLibrary.push(newBook);
 }
 
+function displayBooks () {
+    const grid = document.getElementById("books-grid")
+    grid.innerHTML = "";
+
+    myLibrary.forEach((book) => {
+        const card = document.createElement("div");
+        card.classList.add("book-card");
+        card.dataset.id = book.id;
+
+        card.innerHTML = `
+        <h3>${book.title}</h3>
+        <p><strong>Author:</strong> ${book.author}</p>
+        <p><strong>Pages:</strong> ${book.pages}</p>
+        <p class="read-status ${book.read ? 'read' : 'unread'}">
+            ${book.read ? "Already Read" : "Not Read Yet"}
+        </p>
+        `;
+
+        grid.appendChild(card);
+    });
+}
+
 addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 310, true);
 addBookToLibrary("Harry Potter", "J.K. Rowling", 500, false);
 addBookToLibrary("Clean Code", "Robert C. Martin", 464, true);
 
-console.log(myLibrary);
+displayBooks();
