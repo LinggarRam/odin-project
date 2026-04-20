bf674eb31382fa070e99d98d3e073568dc646000
LinggarRam <123456789+odin@users.noreply.github.com>
2026-04-20 12:17:03 +0700

    Add paragraf footer and styling

--- Library-App/assets/js/main.js
+++ Library-App/assets/js/main.js
@@ -1,40 +1,38 @@
-console.log("Hallo");
+const myLibrary = [];
 
-const myLibrary = []
-
-function Book (title, author, pages, read) {
-    this.id = crypto.randomUUID();
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
+function Book(title, author, pages, read) {
+  this.id = crypto.randomUUID();
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
 }
 
 function addBookToLibrary(title, author, pages, read) {
-    const newBook = new Book(title, author, pages, read);
-    myLibrary.push(newBook);
+  const newBook = new Book(title, author, pages, read);
+  myLibrary.push(newBook);
 }
 
-function displayBooks () {
-    const grid = document.getElementById("books-grid")
-    grid.innerHTML = "";
+function displayBooks() {
+  const grid = document.getElementById("books-grid");
+  grid.innerHTML = "";
 
-    myLibrary.forEach((book) => {
-        const card = document.createElement("div");
-        card.classList.add("book-card");
-        card.dataset.id = book.id;
+  myLibrary.forEach((book) => {
+    const card = document.createElement("div");
+    card.classList.add("book-card");
+    card.dataset.id = book.id;
 
-        card.innerHTML = `
+    card.innerHTML = `
         <h3>${book.title}</h3>
         <p><strong>Author:</strong> ${book.author}</p>
         <p><strong>Pages:</strong> ${book.pages}</p>
-        <p class="read-status ${book.read ? 'read' : 'unread'}">
+        <p class="read-status ${book.read ? "read" : "unread"}">
             ${book.read ? "Already Read" : "Not Read Yet"}
         </p>
         `;
 
-        grid.appendChild(card);
-    });
+    grid.appendChild(card);
+  });
 }
 
 addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 310, true);
