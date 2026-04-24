30997b2bac01c2a2a1f3f1ea8fa81dc10610e850
LinggarRam <123456789+odin@users.noreply.github.com>
2026-04-24 20:34:58 +0700

    create webpack.config.js, .gitignorr, add module.exports

---
+++ 2.Full-Stack-Javascript/6.Todo-List/webpack.config.js
@@ -0,0 +1,35 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: "./src/index.js",
+
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, "dist"),
+        clean: true,
+    },
+    
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+        ],
+    },
+
+    plugin: [
+        new HtmlWebpackPlugin({
+            template: "./src/template.html",
+        }),
+    ],
+
+    devServer: {
+        static: "./dist",
+        port: 8080,
+        opne: true,
+    },
+
+    mode: "development",
+};
