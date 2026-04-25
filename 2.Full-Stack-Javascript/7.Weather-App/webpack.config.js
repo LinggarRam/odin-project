125e7e464b257ae3d6c9336645a6bbeba911b416
LinggarRam <123456789+odin@users.noreply.github.com>
2026-04-25 22:50:50 +0700

    add header html and styling header

--- 2.Full-Stack-Javascript/7.Weather-App/webpack.config.js
+++ 2.Full-Stack-Javascript/7.Weather-App/webpack.config.js
@@ -14,12 +14,12 @@ module.exports = {
         rules: [
             {
                 test: /\.css$/i,
-                use: ["style-loader", " css-loader"],
+                use: ["style-loader", "css-loader"],
             },
         ],
     },
 
-    plugin: [
+    plugins: [
         new HtmlWebpackPlugin({
             template: "./src/template.html",
         }),
