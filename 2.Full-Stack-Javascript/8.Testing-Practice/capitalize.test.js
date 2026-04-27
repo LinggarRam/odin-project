56ebae98889c44d39ad92e98cf4eff81a159fa5e
LinggarRam <123456789+odin@users.noreply.github.com>
2026-04-27 14:17:06 +0700

    add first test TDD

---
+++ 2.Full-Stack-Javascript/8.Testing-Practice/capitalize.test.js
@@ -0,0 +1,13 @@
+import capitalize from "./capitalize";
+
+test("mengkapitalkan huruf pertama", () => {
+  expect(capitalize("hello")).toBe("Hello");
+});
+
+test("string kosong tidak error", () => {
+  expect(capitalize("")).toBe("");
+});
+
+test("huruf kapital diawal tetap sama", () => {
+  expect(capitalize("World")).toBe("World");
+});
