9c28e75cb76dc3c8edb4217e112951cf83a271b0
LinggarRam <123456789+odin@users.noreply.github.com>
2026-04-30 10:20:11 +0700

    add fibsRec function recursive

---
+++ 2.Full-Stack-Javascript/9.Recursion/fibonacci.js
@@ -0,0 +1,38 @@
+// FIBS ITERASI 
+
+// function fibs(n) {
+//     if (n <= 0) return [];
+//     if (n === 1) return [0];
+//     if (n === 2) return [0, 1];
+
+//     const result = [0, 1];
+
+//     for (let i = 2; i < n; i++) {
+//         const next = result[i -1] + result[i -2];
+//         result.push(next);
+//     }
+
+//     return result;
+// }
+
+// console.log("fibs(1)",fibs(1));
+// console.log("fibs(2)",fibs(2));
+// console.log("fibs(8)",fibs(8));
+// console.log("fibs(12)",fibs(12));
+
+function fibsRec(n) {
+    console.log("Hasil Output Recursive");
+
+    if (n <= 0) return [];
+    if (n === 1) return [0];
+    if (n === 2) return [0, 1];
+
+    const prevSequence = fibsRec (n -1);
+
+    const len = prevSequence.length;
+    const nextNumber = prevSequence[len - 1] + prevSequence[len - 2];
+
+    return [...prevSequence, nextNumber];
+}
+
+console.log("fibsRec(8):", fibsRec(8));
