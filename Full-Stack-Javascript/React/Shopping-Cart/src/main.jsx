86b1fab461f09b61fcc4be74d493225e905c4fed
LinggarRam <123456789+odin@users.noreply.github.com>
2026-05-13 23:54:28 +0700

    Add prop-types depedency, setup folder structur, routing, and NavBar skeleton

--- Full-Stack-Javascript/React/Shopping-Cart/src/main.jsx
+++ Full-Stack-Javascript/React/Shopping-Cart/src/main.jsx
@@ -1,10 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import App from "./App.jsx";
+import Cart from "./pages/Cart/Cart.jsx";
+import Home from "./pages/Home/Home.jsx";
+import Shop from "./pages/Shop/Shop.jsx";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: "cart", element: <Cart /> },
+      { path: "shop", element: <Shop /> },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
-)
+);
