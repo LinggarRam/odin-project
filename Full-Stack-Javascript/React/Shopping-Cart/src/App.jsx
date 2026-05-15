28e3ef8004554f64d3d63948693716c31cfd1948
LinggarRam <123456789+odin@users.noreply.github.com>
2026-05-15 23:59:17 +0700

    Add update app

--- Full-Stack-Javascript/React/Shopping-Cart/src/App.jsx
+++ Full-Stack-Javascript/React/Shopping-Cart/src/App.jsx
@@ -1,23 +1,79 @@
-// import { useState } from "react";
+import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import Navbar from "./components/Navbar";
-import "./styles/App.css";
+import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Shop from "./pages/Shop";
 import Cart from "./pages/Cart";
+import "./styles/App.css";
 
 export default function App() {
-  // const [cartItems, setCartItems] = useState([]);
+  const [cartItems, setCartItems] = useState([]);
+
+  const addToCart = (product, qty) => {
+    setCartItems((prev) => {
+      const existing = prev.find((item) => item.id === product.id);
+
+      if (existing) {
+        return prev.map((item) =>
+          item.id === product.id
+            ? { ...item, quantity: item.quantity + qty }
+            : item,
+        );
+      }
+
+      return [
+        ...prev,
+        {
+          id: product.id,
+          title: product.title,
+          price: product.price,
+          image: product.image,
+          quantity: qty,
+        },
+      ];
+    });
+  };
+
+  const updateQty = (id, newQty) => {
+    if (newQty <= 0) {
+      removeFromCart(id);
+      return;
+    }
+    setCartItems((prev) =>
+      prev.map((item) =>
+        item.id === id ? { ...item, quantity: newQty } : item,
+      ),
+    );
+  };
+
+  const removeFromCart = (id) => {
+    setCartItems((prev) => prev.filter((item) => item.id != id));
+  };
+
+  const totalItems = cartItems.reduce((sum, item) => sum + item.quantity, 0);
+
   return (
     <BrowserRouter>
       <div className="app">
-        {/* <Navbar totalItems={totalItems} />*/}
+        <Navbar totalItems={totalItems} />
 
         <main className="main-content">
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/shop" element={<Shop />} />
-            <Route psth="/cart" element={<Cart />} />
+
+            <Route path="/shop" element={<Shop onAddToCart={addToCart} />} />
+
+            <Route
+              path="/cart"
+              element={
+                <Cart
+                  cartItems={cartItems}
+                  onUpdateQty={updateQty}
+                  onRemove={removeFromCart}
+                />
+              }
+            />
+
             <Route
               path="*"
               element={
